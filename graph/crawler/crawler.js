55240a423708ba1f0316f045d5ca79fe457039f7
Joshua Lawson <joshualawson8@gmail.com>
2025-04-16 14:03:52 -0700

    proper edge creation on multi artist albums

--- graph/crawler/crawler.js
+++ graph/crawler/crawler.js
@@ -1,46 +1,70 @@
 const { createArtist, safeCreateOrUpdateEdge, getArtistByStatus, markArtistStatus, closeDriver, getArtistById } = require('../utils/neo4j-graph-utils');
-const { fetchAccessToken, getArtistFromSearch, getArtistAlbums, getAlbumTracks, getArtistDetails } = require('../utils/spotify-api-utils');
+const { fetchAccessToken, getArtistFromSearch, getArtistAlbumIds, getMultipleAlbums, getArtistDetails } = require('../utils/spotify-api-utils');
 const { formatArtistForDb } = require('../utils/neo4j-graph-utils');
 
 let APICallCount = 0;
+let fetchAlbumCount = 0;
+let newUserCount = 0;
+let fetchAlbumIdsCount = 0;
 
 async function crawlArtist(artistData, accessToken) {
   const artistId = artistData.spotifyId;
+  const artistCache = new Map(); 
   await markArtistStatus(artistId, 'inprogress');
 
-  // change to get ALL albums
-  const albums = await getArtistAlbums(accessToken, artistId);
-  APICallCount++;
-  for (const album of albums) {
-    const tracks = await getAlbumTracks(accessToken, album.id);
+  const albumIds = await getArtistAlbumIds(accessToken, artistId);
+  APICallCount = APICallCount + (Math.ceil(albumIds.length/50))
+  fetchAlbumIdsCount = fetchAlbumIdsCount + (Math.ceil(albumIds.length/50))
+  console.log(`Call Count (fetchIds): ${fetchAlbumIdsCount}`)
+
+  for (let i = 0; i < albumIds.length; i += 20) {
+    const batchIds = albumIds.slice(i, i + 20);
+    console.time("getAlbums");
+    const albumDetails = await getMultipleAlbums(accessToken, batchIds);
+    console.timeEnd("getAlbums");
+    fetchAlbumCount++;
     APICallCount++;
+    console.log(`Call Count (fetching albums): ${fetchAlbumCount}`)
 
-    for (const track of tracks) {
-      const artists = track.artists;
 
-      if (artists.length > 1) {
-        for (const collabArtist of artists) {
-          if (collabArtist.id !== artistId) {
-            const existingArtist = await getArtistById(collabArtist.id); // see if artist exists
-            // check if exists
+    for (const album of albumDetails) {
+      const tracks = album.tracks.items;
+
+      for (const track of tracks) {
+        const artists = track.artists;
+        if (artists.length > 1) {
+          // Create all artist nodes first
+          for (const collabArtist of artists) {
+            console.time("getExistingArtist");
+            const existingArtist = await getArtistById(collabArtist.id);
+            console.timeEnd("getExistingArtist");
             if (!(existingArtist.records.length > 0)) {
-              const collabArtistDetails = await getArtistDetails(accessToken, collabArtist.id);
-              APICallCount++;
+              // const collabArtistDetails = await getArtistDetails(accessToken, collabArtist.id);
+
+              console.log(`Call Count (new user): ${newUserCount}`)
               const newCollabArtist = formatArtistForDb({
                 spotifyId: collabArtist.id,
                 name: collabArtist.name,
-                image: collabArtistDetails.images?.[0]?.url || '',
-                popularity: collabArtistDetails.popularity || 0
+                image:  '', //collabArtistDetails.images?.[0]?.url ||
+                popularity: 0 // collabArtistDetails.popularity ||
               });
               await createArtist(newCollabArtist);
             }
+          }
 
-            await safeCreateOrUpdateEdge(artistId, collabArtist.id, {
-              songUri: track.uri,
-              name: track.name,
-              image: album?.images?.[0]?.url || '',
-              albumUri: album?.uri || ''
-            });
+          // Create edges between all unique pairs (n*(n-1)/2)
+          for (let i = 0; i < artists.length; i++) {
+            for (let j = i + 1; j < artists.length; j++) {
+              const a1 = artists[i].id;
+              const a2 = artists[j].id;
+
+              await safeCreateOrUpdateEdge(a1, a2, {
+                songUri: track.uri,
+                name: track.name,
+                image: album?.images?.[0]?.url || '',
+                albumUri: album?.uri || ''
+              });
+            }
           }
         }
       }
@@ -51,16 +75,16 @@ async function crawlArtist(artistData, accessToken) {
 }
 
 async function crawler(seedName = null) {
-  console.log(`Call Count: ${APICallCount}`)
+  console.log(`Call Count TOTAL: ${APICallCount}`)
 
-  const accessToken = await fetchAccessToken(); // 1 hr
+  const accessToken = await fetchAccessToken();
 
   let artistToCrawl;
   if (seedName) {
     const searchResults = await getArtistFromSearch(accessToken, seedName);
     const picked = searchResults.artists.items[0];
     const formatted = formatArtistForDb({
-      spotifyUri: picked.uri,
+      spotifyId: picked.id,
       name: picked.name,
       image: picked.images?.[0]?.url || '',
       popularity: picked.popularity || null
@@ -78,7 +102,7 @@ async function crawler(seedName = null) {
 
   console.log("🔍 Crawling:", artistToCrawl.name);
   await crawlArtist(artistToCrawl, accessToken);
-  await crawler(); // Recursively crawl next
+  await crawler();
 }
 
 crawler(process.argv[2]).finally(async () => {
